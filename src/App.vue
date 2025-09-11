5c1b0c4ebfa30c79e8c020e82b7b0df2b275b463
Neuromancer <psolsilva@gmail.com>
2025-09-11 20:11:05 -0300

    feat: Ajuste de imagens
    Co-authored-by: Mickael Mesquita <mesquita@aospa.co>

--- src/App.vue
+++ src/App.vue
@@ -8,27 +8,75 @@ import Footer from '@/components/Footer.vue'
   <Header></Header>
 
   <main>
-    <h1>Item 1</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur dolore, enim est facilis harum id ipsa, ipsam libero minima nam, nihil numquam obcaecati officiis quae quia quis reprehenderit sed sequi vitae. Commodi dolor fuga iste nisi porro quam quod recusandae rerum. Ab atque autem commodi dolores dolorum, eos ex facere facilis hic ipsa libero maxime nesciunt, non quibusdam repellendus, sed unde voluptates voluptatum. Adipisci dicta distinctio ea facere nam nobis odio quasi sapiente sint totam. Autem culpa eligendi ex, fugit labore perspiciatis provident quia suscipit vero! Ad enim eveniet ex incidunt modi natus nisi, quae reiciendis sunt vel. Est?</p>
+    <div class="container container--primary">
+      <h1>Item 1</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur dolore,
+        enim est facilis harum id ipsa, ipsam libero minima nam, nihil numquam obcaecati officiis
+        quae quia quis reprehenderit sed sequi vitae. Commodi dolor fuga iste nisi porro quam quod
+        recusandae rerum. Ab atque autem commodi dolores dolorum, eos ex facere facilis hic ipsa
+        libero maxime nesciunt, non quibusdam repellendus, sed unde voluptates voluptatum. Adipisci
+        dicta distinctio ea facere nam nobis odio quasi sapiente sint totam. Autem culpa eligendi
+        ex, fugit labore perspiciatis provident quia suscipit vero! Ad enim eveniet ex incidunt modi
+        natus nisi, quae reiciendis sunt vel. Est?
+      </p>
+    </div>
     <h1>Item 2</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur dolore, enim est facilis harum id ipsa, ipsam libero minima nam, nihil numquam obcaecati officiis quae quia quis reprehenderit sed sequi vitae. Commodi dolor fuga iste nisi porro quam quod recusandae rerum. Ab atque autem commodi dolores dolorum, eos ex facere facilis hic ipsa libero maxime nesciunt, non quibusdam repellendus, sed unde voluptates voluptatum. Adipisci dicta distinctio ea facere nam nobis odio quasi sapiente sint totam. Autem culpa eligendi ex, fugit labore perspiciatis provident quia suscipit vero! Ad enim eveniet ex incidunt modi natus nisi, quae reiciendis sunt vel. Est?</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur dolore, enim
+      est facilis harum id ipsa, ipsam libero minima nam, nihil numquam obcaecati officiis quae quia
+      quis reprehenderit sed sequi vitae. Commodi dolor fuga iste nisi porro quam quod recusandae
+      rerum. Ab atque autem commodi dolores dolorum, eos ex facere facilis hic ipsa libero maxime
+      nesciunt, non quibusdam repellendus, sed unde voluptates voluptatum. Adipisci dicta distinctio
+      ea facere nam nobis odio quasi sapiente sint totam. Autem culpa eligendi ex, fugit labore
+      perspiciatis provident quia suscipit vero! Ad enim eveniet ex incidunt modi natus nisi, quae
+      reiciendis sunt vel. Est?
+    </p>
     <h1>Item 3</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur dolore, enim est facilis harum id ipsa, ipsam libero minima nam, nihil numquam obcaecati officiis quae quia quis reprehenderit sed sequi vitae. Commodi dolor fuga iste nisi porro quam quod recusandae rerum. Ab atque autem commodi dolores dolorum, eos ex facere facilis hic ipsa libero maxime nesciunt, non quibusdam repellendus, sed unde voluptates voluptatum. Adipisci dicta distinctio ea facere nam nobis odio quasi sapiente sint totam. Autem culpa eligendi ex, fugit labore perspiciatis provident quia suscipit vero! Ad enim eveniet ex incidunt modi natus nisi, quae reiciendis sunt vel. Est?</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur dolore, enim
+      est facilis harum id ipsa, ipsam libero minima nam, nihil numquam obcaecati officiis quae quia
+      quis reprehenderit sed sequi vitae. Commodi dolor fuga iste nisi porro quam quod recusandae
+      rerum. Ab atque autem commodi dolores dolorum, eos ex facere facilis hic ipsa libero maxime
+      nesciunt, non quibusdam repellendus, sed unde voluptates voluptatum. Adipisci dicta distinctio
+      ea facere nam nobis odio quasi sapiente sint totam. Autem culpa eligendi ex, fugit labore
+      perspiciatis provident quia suscipit vero! Ad enim eveniet ex incidunt modi natus nisi, quae
+      reiciendis sunt vel. Est?
+    </p>
     <h1>Item 4</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur dolore, enim est facilis harum id ipsa, ipsam libero minima nam, nihil numquam obcaecati officiis quae quia quis reprehenderit sed sequi vitae. Commodi dolor fuga iste nisi porro quam quod recusandae rerum. Ab atque autem commodi dolores dolorum, eos ex facere facilis hic ipsa libero maxime nesciunt, non quibusdam repellendus, sed unde voluptates voluptatum. Adipisci dicta distinctio ea facere nam nobis odio quasi sapiente sint totam. Autem culpa eligendi ex, fugit labore perspiciatis provident quia suscipit vero! Ad enim eveniet ex incidunt modi natus nisi, quae reiciendis sunt vel. Est?</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur dolore, enim
+      est facilis harum id ipsa, ipsam libero minima nam, nihil numquam obcaecati officiis quae quia
+      quis reprehenderit sed sequi vitae. Commodi dolor fuga iste nisi porro quam quod recusandae
+      rerum. Ab atque autem commodi dolores dolorum, eos ex facere facilis hic ipsa libero maxime
+      nesciunt, non quibusdam repellendus, sed unde voluptates voluptatum. Adipisci dicta distinctio
+      ea facere nam nobis odio quasi sapiente sint totam. Autem culpa eligendi ex, fugit labore
+      perspiciatis provident quia suscipit vero! Ad enim eveniet ex incidunt modi natus nisi, quae
+      reiciendis sunt vel. Est?
+    </p>
     <h1>Item 5</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur dolore, enim est facilis harum id ipsa, ipsam libero minima nam, nihil numquam obcaecati officiis quae quia quis reprehenderit sed sequi vitae. Commodi dolor fuga iste nisi porro quam quod recusandae rerum. Ab atque autem commodi dolores dolorum, eos ex facere facilis hic ipsa libero maxime nesciunt, non quibusdam repellendus, sed unde voluptates voluptatum. Adipisci dicta distinctio ea facere nam nobis odio quasi sapiente sint totam. Autem culpa eligendi ex, fugit labore perspiciatis provident quia suscipit vero! Ad enim eveniet ex incidunt modi natus nisi, quae reiciendis sunt vel. Est?</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur dolore, enim
+      est facilis harum id ipsa, ipsam libero minima nam, nihil numquam obcaecati officiis quae quia
+      quis reprehenderit sed sequi vitae. Commodi dolor fuga iste nisi porro quam quod recusandae
+      rerum. Ab atque autem commodi dolores dolorum, eos ex facere facilis hic ipsa libero maxime
+      nesciunt, non quibusdam repellendus, sed unde voluptates voluptatum. Adipisci dicta distinctio
+      ea facere nam nobis odio quasi sapiente sint totam. Autem culpa eligendi ex, fugit labore
+      perspiciatis provident quia suscipit vero! Ad enim eveniet ex incidunt modi natus nisi, quae
+      reiciendis sunt vel. Est?
+    </p>
   </main>
 
   <Footer />
 </template>
 
 <style scoped>
-
 main {
   padding-top: calc(var(--header-height) + 30px);
   max-width: var(--content-container);
   margin: 0 auto;
 }
 
+.container {
+}
 </style>
