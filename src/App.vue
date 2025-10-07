32480be33e5a1f7bf009286c28783e1eee7cdcd1
Neuromancer <psolsilva@gmail.com>
2025-10-07 20:31:31 -0300

    chore: Componentiza componente de apresentação

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import { ref } from 'vue'
 import PreviewGallery from '@/components/custom/PreviewGallery.vue'
 import Hero from '@/components/custom/Hero.vue'
 import Contact from '@/components/custom/Contact.vue'
+import Presentation from '@/components/custom/Presentation.vue'
 
 const showMenu = ref(false)
 
@@ -24,25 +25,7 @@ function openMenu() {
 
     <!-- Sobre mim -->
     <section class="container content">
-      <div class="presentation">
-        <div class="presentation__image-container">
-          <img class="presentation__image" src="https://picsum.photos/1920/1080" alt="" />
-        </div>
-        <div class="presentation__info">
-          <h2>Sobre Mim</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto at commodi,
-            deleniti dicta dolor doloremque esse et, illum ipsum iure non porro praesentium
-            quibusdam recusandae soluta suscipit ullam vel veniam veritatis? Dignissimos dolor ea
-            ipsam minima qui quod similique? Expedita iusto magnam nisi pariatur quas voluptatem?
-            Aliquid amet aperiam architecto asperiores aspernatur at blanditiis cumque debitis
-            deleniti doloribus earum esse facilis fuga ipsa iure maxime non porro possimus quia
-            quos, ratione reiciendis, repellat repellendus saepe sapiente similique tempora tenetur
-            velit veniam voluptate! A aliquid assumenda beatae ipsa minima molestias nam
-            necessitatibus nostrum odit, quae quas quo quod, sint tenetur.
-          </p>
-        </div>
-      </div>
+      <Presentation />
     </section>
 
     <!-- Galeria -->
@@ -99,43 +82,5 @@ main {
     gap: 30px;
     justify-content: start;
   }
-}
-
-.presentation {
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  gap: 30px;
-
-  &__image-container {
-    flex: 1;
-    display: flex;
-    border-radius: 5px;
-    overflow: hidden;
-  }
-
-  &__image {
-    object-fit: cover;
-    width: 100%;
-  }
-
-  &__info {
-    display: flex;
-    flex: 2;
-    flex-direction: column;
-  }
-}
-
-// nativos
-
-button {
-  cursor: pointer;
-  width: fit-content;
-  background-color: #536b80;
-  color: white;
-  border: none;
-  padding: 20px 30px;
-  border-radius: 5px;
-  text-transform: uppercase;
 }
 </style>
---
+++ src/components/custom/Presentation.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="presentation">
+    <div class="presentation__image-container">
+      <img class="presentation__image" src="https://picsum.photos/1920/1080" alt="" />
+    </div>
+    <div class="presentation__info">
+      <h2>Sobre Mim</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto at commodi, deleniti
+        dicta dolor doloremque esse et, illum ipsum iure non porro praesentium quibusdam recusandae
+        soluta suscipit ullam vel veniam veritatis? Dignissimos dolor ea ipsam minima qui quod
+        similique? Expedita iusto magnam nisi pariatur quas voluptatem? Aliquid amet aperiam
+        architecto asperiores aspernatur at blanditiis cumque debitis deleniti doloribus earum esse
+        facilis fuga ipsa iure maxime non porro possimus quia quos, ratione reiciendis, repellat
+        repellendus saepe sapiente similique tempora tenetur velit veniam voluptate! A aliquid
+        assumenda beatae ipsa minima molestias nam necessitatibus nostrum odit, quae quas quo quod,
+        sint tenetur.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.presentation {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 30px;
+
+  &__image-container {
+    flex: 1;
+    display: flex;
+    border-radius: 5px;
+    overflow: hidden;
+  }
+
+  &__image {
+    object-fit: cover;
+    width: 100%;
+  }
+
+  &__info {
+    display: flex;
+    flex: 2;
+    flex-direction: column;
+  }
+}
+</style>
